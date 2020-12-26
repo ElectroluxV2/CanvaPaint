65d965078f3f6ef9a0711848caf73a9bf38baf99
ElectroluxV2 <electroluxv2@gmail.com>
2020-12-26 03:54:30 +0100

    Full offScreenCanvas implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild, ElementRef, AfterViewInit, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, EventEmitter, OnDestroy, NgZone } from '@angular/core';
 import { SettingsService } from './settings/settings.service';
+import { WorkerMessagesEnum } from './paint/workerMessages.enum';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,40 @@ import { SettingsService } from './settings/settings.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit, OnDestroy {
+
+  constructor(private ngZone: NgZone, private settingsService: SettingsService) {
+    this.paintMainWorker = new Worker('./paint/paint-main.worker.ts', {
+      type: 'module'
+    });
+  }
+
   @ViewChild('mainCanvas', { read: ElementRef }) mainCanvas: ElementRef;
   @ViewChild('predictCanvas', { read: ElementRef }) predictCanvas: ElementRef;
 
   private paintMainWorker: Worker;
+  private animFrameGlobID;
 
   // Emits paint status to toolbar
   public statusEmitter: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor(private settingsService: SettingsService) {
-    this.paintMainWorker = new Worker('./paint/paint-main.worker.ts', {
-      type: 'module'
-    });
+  private static NormalizePoint(event: PointerEvent): Float32Array {
+    // TODO: multi-touch
+    const point = new Float32Array([
+      event.offsetX,
+      event.offsetY
+    ]);
+
+    // Make sure the point does not go beyond the screen
+    point[0] = point[0] > window.innerWidth ? window.innerWidth : point[0];
+    point[0] = point[0] < 0 ? 0 : point[0];
+
+    point[1] = point[1] > window.innerHeight ? window.innerHeight : point[1];
+    point[1] = point[1] < 0 ? 0 : point[1];
+
+    point[0] *= window.devicePixelRatio;
+    point[1] *= window.devicePixelRatio;
+
+    return point;
   }
 
   ngAfterViewInit(): void {
@@ -33,30 +56,74 @@ export class AppComponent implements AfterViewInit, OnDestroy {
     const predictOffCanvas = this.predictCanvas.nativeElement.transferControlToOffscreen();
 
     this.paintMainWorker.postMessage({
+      type: WorkerMessagesEnum.CanvasSetup,
       mainCanvas: mainOffCanvas,
       predictCanvas: predictOffCanvas,
       devicePixelRatio: window.devicePixelRatio
     }, [mainOffCanvas, predictOffCanvas]);
 
+    this.predictCanvas.nativeElement.onpointermove = (event: PointerEvent) => {
+      this.paintMainWorker.postMessage({
+        type: WorkerMessagesEnum.PointerMove,
+        point: AppComponent.NormalizePoint(event)
+      });
+    };
+
+    this.predictCanvas.nativeElement.onpointerdown = (event: PointerEvent) => {
+      this.paintMainWorker.postMessage({
+        type: WorkerMessagesEnum.PointerDown,
+        point: AppComponent.NormalizePoint(event)
+      });
+    };
+
+    this.predictCanvas.nativeElement.onpointerup = (event: PointerEvent) => {
+      this.paintMainWorker.postMessage({
+        type: WorkerMessagesEnum.PointerUp,
+        point: AppComponent.NormalizePoint(event)
+      });
+    };
+
+    this.settingsService.settings.subscribe(newSettings => {
+      this.paintMainWorker.postMessage({
+        type: WorkerMessagesEnum.Settings,
+        newSettings
+      });
+    });
+
     this.paintMainWorker.onmessage = ({ data }) => {
       console.log('From Web Worker:', data);
     };
 
-
-    // this.paint = new Paint(this.mainCanvas.nativeElement, this.predictCanvas.nativeElement);
+    this.AnimationLoop();
 
     /*this.paint.statusEmitter.subscribe((value) => {
       this.statusEmitter.emit(value);
     });*/
   }
 
+  private AnimationLoop(): void {
+
+    this.paintMainWorker.postMessage({
+      type: WorkerMessagesEnum.AnimationFrame
+    });
+
+    this.ngZone.runOutsideAngular(() => {
+      this.animFrameGlobID = window.requestAnimationFrame(this.AnimationLoop.bind(this));
+    });
+  }
+
   ngOnDestroy(): void {
     // this.paint.statusEmitter.unsubscribe();
+    this.settingsService.settings.unsubscribe();
     this.paintMainWorker.terminate();
+    window.cancelAnimationFrame(this.animFrameGlobID);
   }
 
   public changeMode(value: string): void {
-    // this.paint.OnModeChange(value);
+    this.paintMainWorker.postMessage({
+      type: WorkerMessagesEnum.Mode,
+      mode: value
+    });
   }
 
   public undo(): void {
@@ -68,7 +135,9 @@ export class AppComponent implements AfterViewInit, OnDestroy {
   }
 
   public clear(): void {
-    // this.paint.OnClear();
+    this.paintMainWorker.postMessage({
+      type: WorkerMessagesEnum.Clear
+    });
   }
 
   public onResize(event: Event): void {
