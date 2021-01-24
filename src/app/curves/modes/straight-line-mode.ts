6b4a4900313a8ccad4eb2966fe3c6680f4a87abc
ElectroluxV2 <electroluxv2@gmail.com>
2021-01-24 23:36:44 +0100

    Implemented straight line

--- src/app/curves/modes/straight-line-mode.ts
+++ src/app/curves/modes/straight-line-mode.ts
@@ -17,28 +17,38 @@ export class StraightLine {
 
 export class StraightLineMode extends PaintMode {
 
-  private freeLineOccurringNow = false;
+  private straightLineOccurringNow = false;
+  private currentStraightLine: StraightLine;
 
   public static Reproduce(canvas: CanvasRenderingContext2D, compiled: StraightLine): void {
-
+    canvas.beginPath();
+    canvas.moveTo(compiled.start[0], compiled.start[1]);
+    canvas.strokeStyle = compiled.color;
+    canvas.lineWidth = compiled.width;
+    canvas.lineTo(compiled.stop[0], compiled.stop[1]);
+    canvas.stroke();
   }
 
   public OnMoveBegin(point: Float32Array): void {
-    console.log('start');
+    this.straightLineOccurringNow = true;
+    this.currentStraightLine = new StraightLine(this.settings.color, this.settings.width, point, new Float32Array(2));
   }
 
-  public OnLazyUpdate(lastPointer: Float32Array): StraightLine {
-
-   return null;
+  public OnLazyUpdate(lastPointer: Float32Array): void {
+    this.currentStraightLine.stop = lastPointer;
+    this.predictCanvas.clear();
+    StraightLineMode.Reproduce(this.predictCanvas, this.currentStraightLine);
   }
 
   public OnMoveComplete(): StraightLine {
-    return null;
+    this.straightLineOccurringNow = false;
+    this.predictCanvas.clear();
+    StraightLineMode.Reproduce(this.mainCanvas, this.currentStraightLine);
+
+    return this.currentStraightLine;
   }
 
   public OnSettingsUpdate(settings: Settings): void {
     super.OnSettingsUpdate(settings);
-
-
   }
 }
