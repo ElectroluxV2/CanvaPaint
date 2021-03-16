28c57e03e9a8082960197c181beebdd1f668aaa8
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-16 17:46:06 +0100

    Required methods

--- src/app/curves/modes/continuous-straight-line-mode.ts
+++ src/app/curves/modes/continuous-straight-line-mode.ts
@@ -1,7 +1,10 @@
-import { CompiledObject, PaintMode } from './paint-mode';
+import { FreeLine } from './free-line-mode';
+import {CompiledObject, PaintMode} from './paint-mode';
+import {StraightLine} from './straight-line-mode';
 
 export class ContinuousStraightLineMode extends PaintMode {
-  readonly name = 'continuous-line';
+  readonly name = 'continuous-straight-line';
+
   public ReproduceObject(canvas: CanvasRenderingContext2D, object: CompiledObject): void {
 
   }
@@ -12,5 +15,25 @@ export class ContinuousStraightLineMode extends PaintMode {
 
   public ReadObject(data: string): boolean {
     return false;
+  }
+
+  public OnLazyUpdate(lastPointer: Uint32Array): void {
+      throw new Error('Method not implemented.');
+  }
+
+  public OnMoveBegin(point: Uint32Array, button: number): void {
+      throw new Error('Method not implemented.');
+  }
+
+  public OnMoveComplete(pointerHasMoved: boolean, button: number): FreeLine | StraightLine {
+      throw new Error('Method not implemented.');
+  }
+
+  public OnSelected(): void {
+      throw new Error('Method not implemented.');
+  }
+
+  public MakeReady(): void {
+      throw new Error('Method not implemented.');
   }
 }
