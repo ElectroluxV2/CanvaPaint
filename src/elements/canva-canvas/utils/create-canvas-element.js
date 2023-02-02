3c1402e4aea910b750a4e260854fe936410711ca
ElectroluxV2 <electroluxv2@gmail.com>
2023-02-02 19:21:00 +0100

    disable contextmenu on canvas

--- src/elements/canva-canvas/utils/create-canvas-element.js
+++ src/elements/canva-canvas/utils/create-canvas-element.js
@@ -1,5 +1,6 @@
 export const createCanvasElement = id => {
     const canvas = document.createElement('canvas');
     canvas.setAttribute('id', `layer-${id}`);
+    canvas.addEventListener('contextmenu', e => e.preventDefault());
     return canvas;
 };
